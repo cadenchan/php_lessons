4e58474c38d6385528e8a3d5f8e6426939b89374
cadenchan <cadenchan12@gmail.com>
2025-03-28 09:12:45 -0500

    7_php_data_types

--- index.php
+++ index.php
@@ -7,6 +7,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="datatypes.php">Data Types</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">blank</a>
+            </li>
+            <li class="nav-item">
+              
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>  
     <form method="GET">
       <input type="text" name="person">
       <button>SUBMIT</button>
