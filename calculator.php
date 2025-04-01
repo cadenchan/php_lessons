7e58dab511fac3209e284f36980bc6eada51d40b
cadenchan <cadenchan12@gmail.com>
2025-04-01 09:53:38 -0500

    calculator

---
+++ calculator.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link " href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="datatypes.php">Data Types</a>
+                    </li>
+                   
+                    <li class="nav-item">
+                        <a class="nav-link" href="operators.php">Operators</a>
+                    </li>
+                    <li class="nav-item">
+              <a class="nav-link " href="conditionals.php">Conditionals</a>
+            </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="calculator.php">Calculator</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <form action="">
+        <input type="text" name="num1" placeholder="Number 1">
+        <input type="text" name="num2" placeholder="Number 2">
+        <select name="operator" id="">
+            <option >None</option>
+            <option >Add</option>
+            <option >Subtract</option>
+            <option >Multiply</option>
+            <option >Divide</option>
+        </select>
+        <br>
+        <button type="submit" name="submit" value="submit">
+            Calculate
+        </button>
+    </form>
+    <p>Result: </p>
+    <?php
+        if (isset($_GET['submit'])) {
+            $result1 = $_GET['num1'];
+            $result2 = $_GET['num2'];
+            $operator = $_GET['operator'];
+            switch ($operator) {
+                case "None":
+                    echo "You need to select a method.";
+                break;
+                case "Add":
+                    echo $result1 + $result2;
+                break;
+                case "Subtract":
+                    echo $result1 - $result2;
+                break;
+                case "Multiply":
+                    echo $result1 * $result2;
+                break;
+                case "Divide":
+                    echo $result1 / $result2;
+                break;
+
+            }
+        }
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
