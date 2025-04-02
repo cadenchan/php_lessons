615b898d2eabcb566bc56e5c8eeba78e297f9dbb
cadenchan <cadenchan12@gmail.com>
2025-04-02 08:34:04 -0500

    calendar

--- calendar.php
+++ calendar.php
@@ -19,12 +19,11 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link " href="index.php">Home</a>
+                        <a class="nav-link" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="datatypes.php">Data Types</a>
                     </li>
-
                     <li class="nav-item">
                         <a class="nav-link" href="operators.php">Operators</a>
                     </li>
@@ -32,55 +31,17 @@
                         <a class="nav-link " href="conditionals.php">Conditionals</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="calculator.php">Calculator</a>
+                        <a class="nav-link" href="calculator.php">Calculator</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="calendar.php">Calendar</a>
+                        <a class="nav-link active" aria-current="page" href="calendar.php">Calendar</a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
-    <form action="">
-        <input type="text" name="num1" placeholder="Number 1">
-        <input type="text" name="num2" placeholder="Number 2">
-        <select name="operator" id="">
-            <option>None</option>
-            <option>Add</option>
-            <option>Subtract</option>
-            <option>Multiply</option>
-            <option>Divide</option>
-        </select>
-        <br>
-        <button type="submit" name="submit" value="submit">
-            Calculate
-        </button>
-    </form>
-    <p>Result: </p>
     <?php
-        if (isset($_GET['submit'])) {
-            $result1 = $_GET['num1'];
-            $result2 = $_GET['num2'];
-            $operator = $_GET['operator'];
-            switch ($operator) {
-                case "None":
-                    echo "You need to select a method.";
-                break;
-                case "Add":
-                    echo $result1 + $result2;
-                break;
-                case "Subtract":
-                    echo $result1 - $result2;
-                break;
-                case "Multiply":
-                    echo $result1 * $result2;
-                break;
-                case "Divide":
-                    echo $result1 / $result2;
-                break;
-
-            }
-        }
+        $dayOfWeek = date(w)
     ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
--- index.php
+++ index.php
@@ -33,6 +33,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="calculator.php">Calculator</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="calendar.php">Calendar</a>
+                    </li>
                 </ul>
             </div>
         </div>
