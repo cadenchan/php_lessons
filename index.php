4dd8d468a1f8bdc2278122d98c6f4d3fa0ca84a4
cadenchan <cadenchan12@gmail.com>
2025-04-23 10:10:25 -0500

    php 36

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 
 <?php
     include 'includes/header.php';
+    include 'includes/dbh.inc.php';
 ?>
 
 <body class="bg-danger-subtle">
@@ -26,6 +27,9 @@
       echo $name." is incredibly handsome and awesome.";
     */
       ?>
+    <?php
+        $sql = 
+    ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
