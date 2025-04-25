e4832d7565dd65c0b293f07252456b1629df30af
cadenchan <cadenchan12@gmail.com>
2025-04-25 10:12:27 -0500

    php 44 tmstmp-11:00

--- includes/header.php
+++ includes/header.php
@@ -15,48 +15,55 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-dark " data-bs-theme="dark">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="datatypes.php">Data Types</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="operators.php">Operators</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link " href="conditionals.php">Conditionals</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="calculator.php">Calculator</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="calendar.php">Calendar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="loops.php">Loops</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="arrays.php">Arrays</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="functions.php">Functions</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact.php">Contact</a>
-                    </li>
-                </ul>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-dark " data-bs-theme="dark">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="datatypes.php">Data Types</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="operators.php">Operators</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="conditionals.php">Conditionals</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="calculator.php">Calculator</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="calendar.php">Calendar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="loops.php">Loops</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="arrays.php">Arrays</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="functions.php">Functions</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.php">Contact</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
+        </nav>
+        <div>
+            <form action="includes/login.inc.php" method="post">
+                <!-- CONTINUE HERE 11:00-->
+            </form>
         </div>
-    </nav>
+    </header>
 
     <?php
         $_SESSION['username'] = "caden437";
